6352918785ee7145b87ca07091ebf796e6a95b02
ehtiotumolas <ehtiotumolas@gmail.com>
2023-04-24 23:49:10 +0100

    Poles table fixed, now it displays the right position and information. Fixed headers too for both poles and ranking

--- static/table.js
+++ static/table.js
@@ -7,16 +7,16 @@ export const rowsPerPage = 20;
 export async function createTableRanking() {
     try {
         var theader = document.createElement("div");
-        theader.classList.add("table_row", "table_header");
+        theader.classList.add("table_row", "table_header", "table_ranking");
         var headersList = ["&nbsp", "usuario", "puntos", "mensajes", "hilos",];
         for (var i = 0; i < headersList.length; i++) {
             var header = document.createElement("div");
-            header.classList.add("table_cell");
-            if (i == 0) { header.classList.add("table_cell_first") }
-            if (i == 1) { header.classList.add("table_cell_usuario") }
-            if (i == 2) { header.classList.add("table_cell_puntos") }
-            if (i == 3) { header.classList.add("table_cell_mensajes") }
-            if (i == 4) { header.classList.add("table_cell_hilos") }
+            header.classList.add("table_header_ranking");
+            if (i == 0) { header.classList.add("table_header_ranking_first") }
+            if (i == 1) { header.classList.add("table_header_ranking_usuario") }
+            if (i == 2) { header.classList.add("table_header_ranking_puntos") }
+            if (i == 3) { header.classList.add("table_header_ranking_mensajes") }
+            if (i == 4) { header.classList.add("table_header_ranking_hilos") }
             header.innerHTML = headersList[i];
             theader.appendChild(header);
         }
@@ -40,11 +40,11 @@ export async function updateTableRanking() {
             return;
         }
         var userTable = $(".content-table-ranking")[0];
-        $(".table_body").remove();
+        $(".table_body.table_ranking").remove();
         for (let i = desde; i < hasta; i++) {
             var usuario = usuarios[i].usuario;
             const row = document.createElement("div");
-            row.classList.add("table_row", "table_body");
+            row.classList.add("table_row", "table_body", "table_ranking");
             row.addEventListener("click", function () {
                 var selected = false;
                 if (row.classList.contains("selRow")) {
@@ -57,7 +57,7 @@ export async function updateTableRanking() {
 
             row.id = usuario;
             var position = document.createElement("div");
-            var numRows = document.getElementsByClassName("table_row").length;
+            var numRows = $(".table_row.table_ranking").length;
             position.classList.add("table_cell", "table_cell_first");
             position.innerText = numRows + (currentPage * rowsPerPage);
             if (position.innerText == 1) {row.classList.add("first-row")}
@@ -94,16 +94,15 @@ export async function updateTableRanking() {
 export async function createTablePoles() {
     try {
         var theader = document.createElement("div");
-        theader.classList.add("table_row", "table_header");
-        var headersList = ["&nbsp", "usuario", "poles", "más_info"];
+        theader.classList.add("table_row", "table_header", "table_poles");
+        var headersList = ["&nbsp", "usuario", "poles", "+info"];
         for (var i = 0; i < headersList.length; i++) {
             var header = document.createElement("div");
-            header.classList.add("table_cell");
-            if (i == 0) { header.classList.add("table_cell_first") }
-            if (i == 1) { header.classList.add("table_cell_usuario") }
-            if (i == 2) { header.classList.add("table_cell_puntos") }
-            if (i == 3) { header.classList.add("table_cell_mensajes") }
-            if (i == 4) { header.classList.add("table_cell_hilos") }
+            header.classList.add("table_header_poles");
+            if (i == 0) { header.classList.add("table_header_poles_first") }
+            if (i == 1) { header.classList.add("table_header_poles_usuario") }
+            if (i == 2) { header.classList.add("table_header_poles_poles") }
+            if (i == 3) { header.classList.add("table_header_poles_mas_info") }
             header.innerHTML = headersList[i];
             theader.appendChild(header);
         }
@@ -127,7 +126,7 @@ export async function updateTablePoles() {
         for (let i = 0; i < hasta; i++) {
             var usuario = poles[i].usuario;
             const row = document.createElement("div");
-            row.classList.add("table_row", "table_body");
+            row.classList.add("table_row", "table_body", "table_poles");
             row.addEventListener("click", function () {
                 var selected = false;
                 if (row.classList.contains("selRow")) {
@@ -140,7 +139,7 @@ export async function updateTablePoles() {
 
             row.id = usuario;
             var position = document.createElement("div");
-            var numRows = document.getElementsByClassName("table_row").length;
+            var numRows = document.getElementsByClassName("table_poles").length;
             position.classList.add("table_cell", "table_cell_first");
             position.innerText = numRows;
             if (position.innerText == 1) {row.classList.add("first-row")}
@@ -148,15 +147,15 @@ export async function updateTablePoles() {
             if (position.innerText == 3) {row.classList.add("third-row")}
             row.appendChild(position);
             var usuario_ele = document.createElement("div");
-            usuario_ele.classList.add("table_cell_usuario");
+            usuario_ele.classList.add("table_cell_usuario_poles");
             usuario_ele.innerText = usuario;
             row.append(usuario_ele);
             for (let key in poles[i]) {
                 var cell = document.createElement("div");
                 var valido = false;
                 cell.classList.add("table_cell");
-                if (key == "poles") { cell.classList.add("table_cell_puntos"); valido = true; }
-                if (key == "más_info") { cell.classList.add("table_cell_mensajes"); valido = true; }
+                if (key == "poles") { cell.classList.add("table_cell_poles_poles"); valido = true; }
+                if (key == "mas_info") { cell.classList.add("table_cell_mas_info_poles"); valido = true; }
                 if (valido) {
                     cell.innerText = poles[i][key];
                     row.appendChild(cell);
